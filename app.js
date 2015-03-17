1de0dca3658ff5e9807f8f71f9e5e6b10de2b07a
edwardbryant <edward.bryant@gmail.com>
2015-03-17 06:56:24 -0500

    fixes

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 
 $(document).ready(function(){
+    updateGroups();
 
-    updateTotal("need");
-    updateTotal("got");
-
-    $('#need').on('click', '.check', function() {        
+    $('#need').on('click', '.check', function() {      
         var node = $('<i class="fa fa-check-square-o">');
         $(this).find('.fa').remove();
         $(this).append(node);
+        var itemName = $(this).parent().find('.name').text();
         var item = $(this).closest('.item');
         var itemNode = item.get(0);
         item.fadeOut(800, function() {
             item.remove();
-            updateTotal("need");
+            if (duplicateExists(itemName,"got")) {
+                console.log("found same item in got list.");
+                increaseQty(itemName,"got");
+
+            } else {
+                // addNewItem(name);
+                console.log("no dup conflict found");
+            }
+            
+
+            updateGroups();
             $('#got').find('.grocery-list').first().prepend(itemNode);
             $('#got').find('.item').first().fadeIn(800);
-            updateTotal("got");
+            updateGroups();
         });
     })
 
@@ -27,41 +36,61 @@ $(document).ready(function(){
         var itemNode = item.get(0);
         item.fadeOut(800, function() {
             item.remove();
-            updateTotal("got");
+            updateGroups();
             $('#need').find('.grocery-list').first().prepend(itemNode);
             $('#need').find('.item').first().fadeIn(800);
-            updateTotal("need");
+            updateGroups();
         });         
     })
 
     $('#need').on('click', '.remove', function() {
-        $(this).parent().remove();
-        updateTotal("need");
+        $(this).parent().fadeOut(600, function() {
+            $(this).remove();
+            updateGroups();
+        })
     })
 
     $('#got').on('click', '.remove', function() {
-        $(this).parent().remove();
-        updateTotal("got");
+        $(this).parent().fadeOut(600, function() {
+            $(this).remove();
+            updateGroups();
+        })
     })
 
     $('#add-box .col-right').on('click', function() {
         var name = $('#new-item').val();
         // TODO - trim whitespace from user input.
-        // TODO - reject empty user input.
         if (name != "") {
-            if (duplicateExists(name)) {
-                console.log("Item is already on the need list - so will increase qty.");
-                // TODO - add qty increase code.
+            if (duplicateExists(name,"need")) {
+                increaseQty(name,"need");    
             } else {
                 addNewItem(name);   
             }
             $('#new-item').val('');
-            updateTotal("need");
+        } else {
+            // TODO - error message that input is empty.
         }
     })
 });
-function duplicateExists(name) {
-    if ($('#need').find('.name').filter( function(){
+
+// ------ clean ---------
+
+var updateGroups = function() {
+    var groups = ["need","got"];
+    for (i=0;i<groups.length;i++) {
+        var group = groups[i];
+        var total = $('#' + group).find(".item").length;    
+        if (total > 0) {
+            $('#' + group).find(".total").text(total);
+            $('#' + group).find(".empty").hide();   
+        } else {
+            $('#' + group).find(".total").text(total);
+            $('#' + group).find(".empty").show();
+        }
+    }
+}
+var duplicateExists = function(name,group) {
+    if ($('#' + group).find('.name').filter( function(){
         return $(this).text().toLowerCase() === name.toLowerCase();  
     }).length > 0) {
         return true;
@@ -69,21 +98,26 @@ function duplicateExists(name) {
         return false;
     }
 }
-function addNewItem(name) {
-    var newNode = $('<div class="item hidden"><div class="col-left check"><i class="fa fa-square-o"></i></div><div class="col-middle"><span class="name">'+name+'</span> &times; <span class="qty">'+1+'</span></div><div class="col-right remove"><i class="fa fa-times-circle"></i></div></div>');
+var increaseQty = function(name,group) {
+    $.expr[":"].contains = $.expr.createPseudo(function(arg) {
+        return function( elem ) {
+            return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+        };
+    });
+    var target = $("#" + group + " .name:contains('"+name+"')").parent().find('.qty');
+    var qty = target.text();
+    qty = parseInt(qty) + 1;
+    target.text(qty);
+}
+var addNewItem = function(name) {
+    var newNode = $(' '
+        + ' <div class="item hidden"> '
+        + '    <div class="col-left check"><i class="fa fa-square-o"></i></div> '
+        + '    <div class="col-middle"><span class="name">' + name + '</span> &times; <span class="qty">' + 1 + '</span></div> '
+        + '    <div class="col-right remove"><i class="fa fa-times-circle"></i></div> '
+        + ' </div> ');
     $('#need').find('.grocery-list').first().prepend(newNode);
-    $('#need').find('.item').first().fadeIn(800);
+    $('#need').find('.item').first().fadeIn(800, function(){
+        updateGroups();
+    });
 }
-function Total(type) {
-    return $("#" + type).find(".item").length;
-}
-function updateTotal(type) {
-    var total = Total(type);
-    if (total > 0) {
-        $("#" + type).find(".total").text(total);
-        $("#" + type).find(".empty").hide();   
-    } else {
-        $("#" + type).find(".total").text(total);
-        $("#" + type).find(".empty").show();
-    }
-} 
